e1db0b04cc82dbb2d34a5551a939f8bbc5e4fa00
JuanSFL <molina98j@yahoo.com>
2022-11-27 16:46:53 -0500

    file consolidation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // inquirer v8.2.4
 const inquirer = require('inquirer');
 // generateMarkdown.js file
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./generateMarkdown.js');
 
 // questions array
 const questions = [
